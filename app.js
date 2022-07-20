77da9c376180b92ad02d02f2d6541265a9d96aba
Oleksii Petukhov <48577654+a-petukhov@users.noreply.github.com>
2022-07-20 23:06:50 +0300

    Adding Mongo functional to default content & posts

--- app.js
+++ app.js
@@ -1,50 +1,117 @@
-//jshint esversion:6
+//jshint esversion:8
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const date = require(__dirname + '/date.js');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const defaultContent = {
-  home: homeStartingContent,
-  about: aboutContent,
-  contact: contactContent,
-}
-const posts = [];
-
-const dateNow = date.getDate();
-
 const app = express();
+const dateNow = date.getDate();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
 
 mongoose.connect('mongodb+srv://huge-orange-master:3cWvoZjQRbsVEZFqUq@cluster0.arlckwg.mongodb.net/blogwebsiteDB?retryWrites=true&w=majority');
 
+const postSchema = new mongoose.Schema({
+  route: String,
+  title: String,
+  body: String,
+  dateAdded: Date
+});
+const Post = new mongoose.model('Post', postSchema);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  posts: [postSchema]
+});
+const User = new mongoose.model('User', userSchema);
+
+const contentSchema = new mongoose.Schema({
+  body: String,
+  page: String
+});
+const Content = new mongoose.model('Content', contentSchema);
+
+const content1 = new Content({
+  body: 'Dolores rerum architecto laudantium ut est libero qui modi ut quia iste sed veritatis. Optio ut reiciendis eos dolorem dicta aut cupiditate veniam veritatis officia in perferendis distinctio. Et cupiditate at est minima et. Et explicabo sequi totam unde cum deserunt ea. Aperiam earum natus quia porro non voluptate. Officia ut est doloribus placeat eveniet blanditiis quia et ex. Dolores voluptas incidunt natus ut rerum doloribus.',
+  page: 'home'
+});
+const content2 = new Content({
+  body: 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.',
+  page: 'about'
+});
+const content3 = new Content({
+  body: 'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.',
+  page: 'contact'
+});
+const defaultContent = [content1, content2, content3];
+
 app.get('/', (req, res) => {
-  res.render('home', {
-    defaultContent,
-    posts: posts,
+
+  Content.findOne({
+    page: 'home'
+  }, (err, foundContent) => {
+    if (err) {
+      console.log(err);
+
+    } else if (foundContent.length === 0) {
+      Content.insertMany(defaultContent, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+
+      res.redirect('/');
+
+    } else {
+
+      Post.find({}, (err, foundPost) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render('home', {
+            defaultContent: foundContent.body,
+            posts: foundPost
+          });
+        }
+      });
+
+    }
   });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    defaultContent
+  Content.findOne({
+    page: 'about'
+  }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('about', {
+        defaultContent: result.body
+      });
+    }
   });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {
-    defaultContent
+  Content.findOne({
+    page: 'contact'
+  }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('contact', {
+        defaultContent: result.body
+      });
+    }
   });
 });
 
@@ -53,36 +120,40 @@ app.get('/compose', (req, res) => {
 });
 
 app.post('/compose', (req, res) => {
-  const post = {
-    name: req.body.postTitle.toLowerCase().replace(/[^a-z0-9 _-]+/gi, '-').replace(/\s/g , '_'),
+
+  const newPost = new Post({
+    route: req.body.postTitle.toLowerCase().replace(/[^a-z0-9 _-]+/gi, '-').replace(/\s/g, '_'),
     title: req.body.postTitle,
     body: req.body.postBody,
-  }
-  posts.push(post);
+    dateAdded: dateNow
+  });
+  newPost.save();
   res.redirect('/');
 });
 
 app.get('/post/:postNameID', (req, res) => {
-  const requestedParam = req.params.postNameID;
+  const requestedPost = req.params.postNameID;
 
-  posts.forEach(post => {
-    let exists = Object.values(post).includes(requestedParam);
-    if (exists === true) {
-      res.render('post', {
-        title: post.title,
-        body: post.body
-      });
+  Post.findOne({
+    route: requestedPost
+  }, (err, result) => {
+    if (err) {
+      console.log(err);
     } else {
-      res.redirect('/');
+      res.render('post', {
+        title: result.title,
+        body: result.body,
+        dateAdded: result.dateAdded
+      });
     }
   });
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port == '') {
   port = 3000;
 }
 
 app.listen(port, function (req, res) {
-  console.log('The server is runing now.');
+  console.log('The server is runing on port ' + port + '.');
 });
